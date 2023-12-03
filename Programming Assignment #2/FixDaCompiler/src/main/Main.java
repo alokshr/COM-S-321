ee727d312b82d47e66728e52971cf609ca070ea7
AlokS <BinayaShree@gmail.com>
2023-12-03 13:36:54 -0600

    Read binary file

--- Programming Assignment #2/FixDaCompiler/src/main/Main.java
+++ Programming Assignment #2/FixDaCompiler/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,17 +11,24 @@ public class Main {
 			throw new IllegalArgumentException("No arguments given");
 		}
 		
-		File binaryFile = new File(args[0]);
+		//File binaryFile = new File(args[0]);
 		
-		FileInputStream reader;
-		try {
-			reader = new FileInputStream(binaryFile);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			reader.close();
-		}
+		String fileName = args[0]; // Replace with your binary file's name
+
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            int byteRead;
+
+            while ((byteRead = fis.read()) != -1) {
+                String binaryString = Integer.toBinaryString(byteRead);
+                // Ensure each byte is represented as 8 bits (0-padded if necessary)
+                binaryString = String.format("%8s", binaryString).replace(' ', '0');
+                System.out.print(binaryString);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + fileName);
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
 		
 		
 	}
