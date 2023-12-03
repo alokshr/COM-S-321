e047b31eec9d09df3900e3e22863f7fd25faeead
AlokS <BinayaShree@gmail.com>
2023-12-03 14:52:11 -0600

    print out in 32bits

--- Programming Assignment #2/FixDaCompiler/src/main/Main.java
+++ Programming Assignment #2/FixDaCompiler/src/main/Main.java
@@ -5,30 +5,54 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
+
+	public static String byteToBinaryString(byte value) {
+		StringBuilder binaryString = new StringBuilder(8); // Assuming you want 8 bits (1 byte)
+		
+		for (int i = 7; i >= 0; i--) {
+			int bit = (value >> i) & 1; // Shift the bits and extract the least significant bit
+			binaryString.append(bit);
+		}
+		
+		return binaryString.toString();
+	}
 	
 	public static void main(String[] args) throws IOException{
+
 		// Get binary file
 		if (args.length == 0) {
 			throw new IllegalArgumentException("No arguments given");
 		}
-		
-		//File binaryFile = new File(args[0]);
+		FileInputStream fis = null;
 		
 		String fileName = args[0]; // Replace with your binary file's name
 
-        try (FileInputStream fis = new FileInputStream(fileName)) {
-            int byteRead;
-
-            while ((byteRead = fis.read()) != -1) {
-                String binaryString = Integer.toBinaryString(byteRead);
-                // Ensure each byte is represented as 8 bits (0-padded if necessary)
-                binaryString = String.format("%8s", binaryString).replace(' ', '0');
-                System.out.print(binaryString);
+        try {
+			int lineNumber = 0;
+			fis = new FileInputStream(fileName);
+            byte[] buffer = new byte[4]; // Assuming each 32-bit integer is 4 bytes
+			
+            while (fis.read(buffer) != -1) {
+				String _32BString = "";
+				for(byte b : buffer) {
+					_32BString += byteToBinaryString(b);
+				}
+				System.out.println(lineNumber + ": " +_32BString);
+				lineNumber += 1;
             }
+
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + fileName);
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
-        }
+        } finally {
+			try {
+				if(fis != null) {
+					fis.close();
+				}
+			} catch (IOException e) {
+                System.err.println("Error closing the file: " + e.getMessage());
+            }
+		}
 	}
 }
