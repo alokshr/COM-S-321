62f7ea068cf1d6d297b669e58d50c42b91f07c02
alokshr <alokshrest@gmail.com>
2023-12-03 13:32:13 -0600

    Fixed programming assignment #2's file structure

--- Programming Assignment #2/FixDaCompiler/src/main/Main.java
+++ Programming Assignment #2/FixDaCompiler/src/main/Main.java
@@ -3,25 +3,31 @@ package main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException{
+		// Get binary file
 		if (args.length == 0) {
 			throw new IllegalArgumentException("No arguments given");
 		}
 		
 		File binaryFile = new File(args[0]);
 		
-		FileInputStream reader;
+		// Open stream to read the bits of the file
+		FileInputStream reader = null;
+		
 		try {
 			reader = new FileInputStream(binaryFile);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			reader.close();
 		}
 		
+		byte[] instructionLine = new byte[4];
 		
+		reader.read(instructionLine);
 	}
 }
